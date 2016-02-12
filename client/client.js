6112e3cb7e33825073062c7c8b3bdf5bc9164c3e
Lee Owens <leeaaronowens@gmail.com>
2016-02-12 03:54:16 +0000

    refactoring

--- client/client.js
+++ client/client.js
@@ -39,45 +39,4 @@
     passwordSignupFields: "USERNAME_ONLY"
   });
   
-  Template.Contacts_show.helpers({
-    contacts: function(){
-			return Contacts.find();
-		}
-    
-  });
-
-   Template.Contacts_show.events({
-    "submit .new-contact": function (event) {
-
-      // Prevent default browser form submit
-      event.preventDefault();
-
-      // Get value from form element
-      var nameProp = event.target.name.value;
-	  var phoneProp = event.target.phone.value;
-
-      // Insert a task into the collection
-     // Insert a task into the collection
-      Contacts.insert({
-        name: nameProp,
-		phone: phoneProp,
-		createdBy:  Meteor.user().username,
-		lastCalled: null,
-        createdAt: new Date() // current time
-      });
-
-      // Clear form
-      event.target.name.value = "";
-	  event.target.phone.value = "";
-     }
-   });
-
-   Template.contact.events({
-    "click .call": function () {
-
-	document.location.href = 'tel:'+this.phone;
-	Contacts.update(this._id, {
-        $set: {lastCalled: new Date()}
-      });
-    }
-  });
+  
