0df11e663290e7a73ff1b1ba000fd243a4220350
Lee Owens <leeaaronowens@gmail.com>
2016-02-24 02:18:19 +0000

    More fields for contacts

--- client/contacts/contacts.js
+++ client/contacts/contacts.js
@@ -1,103 +1,129 @@
+  /* global FlowRouter, BlazeLayout */
   FlowRouter.route('/contacts/new', {
     name: 'Contacts.show',
     action() {
       newContact();
     }
   });
-  
-    FlowRouter.route('/contacts/edit/:contactId', {
+
+  FlowRouter.route('/contacts/edit/:contactId', {
     name: 'Contacts.edit',
     action(params) {
       editContact(params.contactId);
     }
   });
 
-Template.Contacts_show.helpers({
-  contacts: function() {
-    return Contacts.find();
-  }
-});
+  Template.Contacts_show.helpers({
+    contacts: function() {
+      return Contacts.find();
+    }
+  });
 
-Template.contact_form.helpers({
-  verb: function(){
-    if(editContactId) { return 'Edit';} else { return 'Create'; }
-  },
-  editMode: function(){ return (editContactId !== null);},
-  name: function(){
-    if(editContactId) {
-      return Contacts.findOne({_id: editContactId}).name;
-    } else return "";
-  },
-  phone: function(){
-    if(editContactId) {
-      return Contacts.findOne({_id: editContactId}).phone;
-    } else return "";
-  }
-});
+  var editContactId = null;
 
-Template.contact_form.onRendered(function() {
-  $('.float-label-control').floatLabels();
-  $('.float-label-control').trigger('change');
-});
-editContactId = null;
-
-function editContact(contactId) {
-  editContactId = contactId;
-        BlazeLayout.render('App_body', {
-        main: 'contact_form'
-      });
-}
-
-function newContact(){
-  editContactId = null;
-        BlazeLayout.render('App_body', {
-        main: 'contact_form'
-      });
-}
-
-
-Template.contact_form.events({
-  "click #cancelCreateContact": function() {
-    document.location = "/contacts";
-  },
-  "click #deleteContact" : function(){
-    Contacts.remove(editContactId);
-  },
-  "submit .new-contact": function(event) {
-
-    // Prevent default browser form submit
-    event.preventDefault();
-
-    // Get value from form element
-    var nameProp = event.target.name.value;
-    var phoneProp = event.target.phone.value;
-
-  if(editContactId) {
-    Contacts.update(editContactId, {
-      $set: {
-        name: nameProp,
-        phone: phoneProp
+  Template.contact_form.helpers({
+    verb: function() {
+      if (editContactId) {
+        return 'Edit';
       }
-    });
-  } else {
-    Contacts.insert({
-      name: nameProp,
-      phone: phoneProp,
-      createdBy: Meteor.user().username,
-      lastCalled: null,
-      everCalled: false,
-      createdAt: new Date() // current time
+      else {
+        return 'Create';
+      }
+    },
+    editMode: function() {
+      return (editContactId !== null);
+    },
+
+  });
+
+  function editContact(contactId) {
+    editContactId = contactId;
+    BlazeLayout.render('App_body', {
+      main: 'contact_form'
     });
   }
-    
 
-    // Clear form
-    event.target.name.value = "";
-    event.target.phone.value = "";
-    document.location = "/contacts";
+  function newContact() {
+    editContactId = null;
+    BlazeLayout.render('App_body', {
+      main: 'contact_form'
+    });
   }
-});
 
-Template.contact.events({
+  Template.contact_form.onRendered(function() {
+    if (editContactId) {
+      
+      var myContact = Contacts.findOne({
+        _id: editContactId
+      });
+      if (myContact) {
+        $('input[name=name]').val(myContact.name);
+        $('input[name=phone]').val(myContact.phone);
+        $('input[name=company]').val(myContact.company);
+        $('input[name=role]').val(myContact.role);
+        $('input[name=email]').val(myContact.email);
+      }
+    }
+    $('.float-label-control').floatLabels();
+    $('.float-label-control').trigger('change');
+  });
 
-});
+
+  Template.contact_form.events({
+    "click #cancelCreateContact": function() {
+      document.location = "/contacts";
+    },
+    "click #deleteContact": function() {
+      Contacts.remove(editContactId);
+    },
+    "submit .new-contact": function(event) {
+
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var nameProp = event.target.name.value;
+      var phoneProp = event.target.phone.value;
+      var emailProp = event.target.email.value;
+      var companyProp = event.target.company.value;
+      var roleProp = event.target.role.value;
+
+      if (editContactId) {
+        Contacts.update(editContactId, {
+          $set: {
+            name: nameProp,
+            phone: phoneProp,
+            email: emailProp,
+            company: companyProp,
+            role: roleProp
+          }
+        });
+      }
+      else {
+        Contacts.insert({
+          name: nameProp,
+          phone: phoneProp,
+          email: emailProp,
+          company: companyProp,
+          role: roleProp,
+          createdBy: Meteor.user().username,
+          lastCalled: null,
+          everCalled: false,
+          createdAt: new Date() // current time
+        });
+      }
+
+
+      // Clear form
+      event.target.name.value = "";
+      event.target.phone.value = "";
+      event.target.company.value = "";
+      event.target.role.value = "";
+      event.target.email.value = "";
+      document.location = "/contacts";
+    }
+  });
+
+  Template.contact.events({
+
+  });
